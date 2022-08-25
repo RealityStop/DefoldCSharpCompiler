e69be4b28847c7de29b995077c37de64342296c3
RealityStop <realitydotstop@gmail.com>
2022-08-25 09:07:50 -0400

    updated test batch files to use the .net 6 runtime the rest of the project is using

--- test/fibonacci/cmd.bat
+++ test/fibonacci/cmd.bat
@@ -1,3 +1,6 @@
-set dir=../../CSharp.lua.Launcher/bin/Debug/net5.0/
+set dir=../../CSharp.lua.Launcher/bin/Debug/net6.0/
 dotnet "%dir%CSharp.lua.Launcher.dll" -s src -d out
 "../__bin/lua5.1/lua" launcher.lua
+
+
+pause
